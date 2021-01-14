552e40c5a92a24be3f0e5d0706ed259abeecd9e8
Verena Traub <verena@yngage.de>
2021-01-14 19:24:31 +0100

    lab mostly done, small pieces of iteration 2 and 4 still in process

--- index.js
+++ index.js
@@ -14,14 +14,61 @@ mongoose
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
-  .then(self => {
+  .then((self) => {
     console.log(`Connected to the database: "${self.connection.name}"`);
     // Before adding any documents to the database, let's delete all previous entries
     return self.connection.dropDatabase();
   })
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    /* ITERATION 2:
+      return Recipe.create({
+      title: 'Asian Glazed Chicken Thighs',
+      level: 'Amateur Chef',
+      ingredients: [
+        '1/2 cup rice vinegar',
+        '5 tablespoons honey',
+        '1/3 cup soy sauce (such as Silver Swan®)',
+        '1/4 cup Asian (toasted) sesame oil',
+        '3 tablespoons Asian chili garlic sauce',
+        '3 tablespoons minced garlic',
+        'salt to taste',
+        '8 skinless, boneless chicken thighs'
+      ],
+      cuisine: 'Asian',
+      dishType: 'main_course',
+      image:
+        'https://images.media-allrecipes.com/userphotos/720x405/815964.jpg',
+      duration: 40,
+      creator: 'Chef LePapu'
+    });
+  })*/
+    /* ITERATION 2:
+  .then((recipe) => {
+    console.log('Recipe was created');
+    console.log(recipe.title); */
+    return Recipe.insertMany(data);
   })
-  .catch(error => {
+  .then((data) => {
+    console.log('Several recipes were created');
+    // console.log( { title } );  // still need to figure out
+
+    return Recipe.findByIdAndUpdate('6000882b3f6bcf0889a453a4', {
+      duration: 100 // not working
+    });
+  })
+  .then((updatedRecipe) => {
+    console.log('Recipe was updated');
+
+    return Recipe.findOneAndDelete({ title: 'Carrot Cake' });
+  })
+  .then(() => {
+    console.log('Recipe was deleted');
+
+    return mongoose.disconnect();
+  })
+  .then(() => {
+    console.log('Connection has been closed');
+  })
+  .catch((error) => {
     console.error('Error connecting to the database', error);
   });
